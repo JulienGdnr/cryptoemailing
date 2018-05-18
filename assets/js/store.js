361917736eccca6bef9258f00774210dffebce50
JulienGdnr <julien.godenir.pro@gmail.com>
2018-05-17 19:38:49 -0600

    lowercase

--- assets/js/store.js
+++ assets/js/store.js
@@ -55,7 +55,7 @@ class Store {
   
     // Create an order object to represent the line items.
     async createOrder(email, source) {
-      email = email.tolowercase()
+      email = email.toLowerCase()
       try {
         const response = await fetch('https://api.datapony.co/cryptos/order', {
           method: 'POST',
